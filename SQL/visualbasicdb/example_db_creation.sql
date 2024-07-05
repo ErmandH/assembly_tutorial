2937b8a294a1f5f1ebf0f1536e8edb8aec2b61e2
Ermand Haruni <88191097+ErmandH@users.noreply.github.com>
2024-07-05 14:48:23 +0300

    banksoft_notlar

---
+++ SQL/visualbasicdb/example_db_creation.sql
@@ -0,0 +1,45 @@
+-- Ermand Haruni 02/07/2024
+
+
+
+-- Patient table creation
+
+if exists (select 1 from sys.tables where name = 'Patients')
+	drop table Patients
+
+create table Patients (
+	PatientID int identity(1,1) PRIMARY KEY,
+	FirstName varchar(100),
+	LastName varchar(100),
+	Gender varchar(5),
+	PhoneNumber varchar(30)
+)
+
+
+-- Doctor table creation
+if exists (select 1 from sys.tables where name = 'Doctors')
+	drop table Doctors
+
+create table Doctors (
+	DoctorID int identity(1,1) PRIMARY KEY,
+    FirstName varchar(50),
+    LastName varchar(50),
+    PhoneNumber varchar(30),
+    Email varchar(100),
+	Branch varchar(100),
+)
+
+
+-- Appointment table creation
+if exists (select 1 from sys.tables where name = 'Appointments')
+	drop table Appointments
+
+create table Appointments (
+	AppointmentID int identity(1,1) PRIMARY KEY,
+    PatientID int,
+    DoctorID int,
+    AppointmentDateTime datetime,
+    Notes varchar(MAX),
+    FOREIGN KEY (PatientID) REFERENCES Patients(PatientID),
+    FOREIGN KEY (DoctorID) REFERENCES Doctors(DoctorID)
+)
